77e5494576f9c7ec5a4f6641deee199951e4ebcc
AdamBalazsKovacs <dbobo4@gmail.com>
2022-08-25 19:51:28 +0200

    first commit

---
+++ src/main/java/hu/dbobo/Main.java
@@ -0,0 +1,7 @@
+package hu.dbobo;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/main/java/hu/dbobo/manager/DerbyConfig.java
@@ -0,0 +1,125 @@
+package hu.dbobo.manager;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class DerbyConfig {
+    private static final String URL = "jdbc:derby:VALUTA;create=true";
+
+    private static Connection getConnection() throws SQLException, ClassNotFoundException {
+        Class.forName("org.apache.derby.jdbc.ClientDriver");
+        Connection connection = DriverManager.getConnection(URL);
+        return connection;
+    }
+
+    public static void initDatabase() {
+        String sqlValuta = """
+                CREATE TABLE valuta (
+                valuta_id VARCHAR(3) NOT NULL,
+                valuta_name VARCHAR(255) NOT NULL,
+                                
+                CONSTRAINT valuta_pk PRIMARY KEY (valuta_id)
+                )
+                """;
+
+        String sqlExchangeRate = """
+                CREATE TABLE exchange_rate (
+                valuta_id VARCHAR(3) NOT NULL,
+                chf DOUBLE NOT NULL,
+                eur DOUBLE NOT NUll;
+                usd DOUBLE NOT NUll;
+                huf DOUBLE NOT NUll;
+                                
+                CONSTRAINT exchange_rate_pk PRIMARY KEY (valuta_id),
+                CONSTRAINT valuta_id_fk FOREIGN KEY (valuta_id) REFERENCES VALUTA (valuta_id)
+                )
+                """;
+
+        String sqlChange = """
+                CREATE TABLE change_event (
+                change_id INTEGER NOT NULL,
+                valuta_from VARCHAR(3) NOT NULL,
+                valuta_to VARCHAR(3) NOT NULL,
+                amount DOUBLE NOT NULL,
+                change_date DATE NOT NULL,
+                                
+                CONSTRAINT change_pk PRIMARY KEY (change_id),
+                CONSTRAINT from_fk FOREIGN KEY (valuta_from) REFERENCES VALUTA (valuta_id),
+                CONSTRAINT to_fk FOREIGN KEY (valuta_to) REFERENCES VALUTA (valuta_id)
+                )
+                """;
+
+        String[] createTableSqls = {sqlValuta, sqlExchangeRate, sqlChange};
+        String[] createTableNames = {"valuta", "exchange_rate", "change_event"};
+
+        for (int i = 0; i < createTableNames.length; i++) {
+            createTable(createTableSqls[i], createTableNames[i]);
+        }
+
+
+
+    }
+
+    private static void insertValuta(String valutaId, String valutaName) {
+        String sql = """
+                INSERT INTO VALUTA (valuta_id, valuta_name) VALUES (?,?)
+                """;
+
+        try (Connection connection = getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
+
+            preparedStatement.setString(1, valutaId);
+            preparedStatement.setString(2, valutaName);
+
+            preparedStatement.executeUpdate();
+
+        } catch (SQLException | ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void createTable(String sql, String tablaNev) {
+        if (!isTablePresent(tablaNev)) {
+            try (Connection connection = getConnection();
+                 Statement statement = connection.createStatement()) {
+                statement.executeQuery(sql);
+            } catch (SQLException | ClassNotFoundException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private static boolean isTablePresent(String tablaNev) {
+        try (Connection connection = getConnection()) {
+            DatabaseMetaData databaseMetaData = connection.getMetaData();
+
+            try (ResultSet resultSet = databaseMetaData.getTables(null, null, null, new String[]{"TABLE"})) {
+                while (resultSet.next()) {
+                    String tableName = resultSet.getString("TABLE_NAME");
+                    if (tableName.equalsIgnoreCase(tablaNev)) {
+                        return true;
+                    }
+                }
+            }
+        } catch (SQLException | ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
