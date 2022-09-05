bda2d67648d0428735990ea1ac0dc6c751fe8a41
AdamBalazsKovacs <dbobo4@gmail.com>
2022-09-05 21:55:09 +0200

    Fix return types in implementation

--- src/main/java/hu/dbobo/controller/ExchangeServiceImpl.java
+++ src/main/java/hu/dbobo/controller/ExchangeServiceImpl.java
@@ -7,6 +7,7 @@ import hu.dbobo.model.valuta.ValutaId;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
+import java.util.Optional;
 
 public class ExchangeServiceImpl implements ExchangeService {
 
@@ -19,19 +20,19 @@ public class ExchangeServiceImpl implements ExchangeService {
     }
 
     @Override
-    public BigDecimal exchange(ValutaId from, ValutaId to, BigDecimal amount) {
+    public Optional<BigDecimal> exchange(ValutaId from, ValutaId to, BigDecimal amount) {
         double currentRate = valutaCache.getExchangeRate(from, to);
         BigDecimal amountBigDecimal = new BigDecimal(String.valueOf(amount));
         ChangeEvent changeEvent = new ChangeEvent(from, to, amount.doubleValue(), LocalDate.now());
         changeEventManager.insertEvent(changeEvent);
-        return amountBigDecimal.multiply(BigDecimal.valueOf(currentRate));
+        return Optional.of(amountBigDecimal.multiply(BigDecimal.valueOf(currentRate)));
     }
 
     @Override
-    public BigDecimal checkRate(ValutaId from, ValutaId to, BigDecimal amount) {
+    public Optional<BigDecimal> checkRate(ValutaId from, ValutaId to, BigDecimal amount) {
         double currentRate = valutaCache.getExchangeRate(from, to);
         BigDecimal amountBigDecimal = new BigDecimal(String.valueOf(amount));
-        return amountBigDecimal.multiply(BigDecimal.valueOf(currentRate));
+        return Optional.of(amountBigDecimal.multiply(BigDecimal.valueOf(currentRate)));
     }
 
     @Override
