9e4816c29ca5892104a5a24c99c72f07156ea9dc
AdamBalazsKovacs <dbobo4@gmail.com>
2022-09-05 21:51:26 +0200

    Change return types to Optionals

--- src/main/java/hu/dbobo/controller/ExchangeService.java
+++ src/main/java/hu/dbobo/controller/ExchangeService.java
@@ -3,12 +3,13 @@ package hu.dbobo.controller;
 import hu.dbobo.model.valuta.ValutaId;
 
 import java.math.BigDecimal;
+import java.util.Optional;
 
 public interface ExchangeService {
 
-    BigDecimal exchange(ValutaId from, ValutaId to, BigDecimal amount);
+    Optional<BigDecimal> exchange(ValutaId from, ValutaId to, BigDecimal amount);
 
-    BigDecimal checkRate(ValutaId from, ValutaId to, BigDecimal amount);
+    Optional<BigDecimal> checkRate(ValutaId from, ValutaId to, BigDecimal amount);
 
     boolean setExchangeRate(ValutaId from, ValutaId to, double rate);
 
