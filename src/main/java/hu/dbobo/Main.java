a14e649058dd5eb108f67f8c8ae6de3a357eae8b
Papp Szabolcs <papp.szabolcs.bazil@gmail.com>
2022-09-05 21:20:10 +0200

    Add skeletal GUI implementation

--- src/main/java/hu/dbobo/Main.java
+++ src/main/java/hu/dbobo/Main.java
@@ -2,6 +2,7 @@ package hu.dbobo;
 
 import hu.dbobo.config.DerbyConfig;
 import hu.dbobo.model.valuta.ValutaCache;
+import hu.dbobo.view.AppFrame;
 
 public class Main {
     public static void main(String[] args) {
@@ -11,6 +12,8 @@ public class Main {
         ValutaCache valutaCache = ValutaCache.getInstance();
         // TODO: Instantiate a ChangeEventManager/ChangeEventRepository.
         // TODO: Instantiate an ExchangeService implementation.
+
+        new AppFrame(null);
 
     }
 }
--- src/main/java/hu/dbobo/view/AppFrame.java
+++ src/main/java/hu/dbobo/view/AppFrame.java
@@ -1,13 +1,78 @@
 package hu.dbobo.view;
 
 import hu.dbobo.controller.ExchangeService;
+import hu.dbobo.model.valuta.ValutaId;
 
 import javax.swing.*;
+import java.awt.*;
+import java.math.BigDecimal;
 
 public class AppFrame extends JFrame {
 
     private final ExchangeService exchangeService;
+
     public AppFrame(ExchangeService exchangeService) {
         this.exchangeService = exchangeService;
+        initAppearance();
+
+        JLabel fromLabel = new JLabel("from");
+        ValutaComboBox fromPick = new ValutaComboBox();
+
+        JLabel toLabel = new JLabel("from");
+        ValutaComboBox toPick = new ValutaComboBox();
+
+        JLabel amountLabel = new JLabel("amount");
+        JTextField amountField = new JTextField(20);
+
+        JButton okBtn = new JButton("OK");
+        okBtn.addActionListener(e -> {
+            ValutaId from = fromPick.getSelection();
+            ValutaId to = toPick.getSelection();
+            BigDecimal amount;
+            try {
+                amount = new BigDecimal(amountField.getText());
+            } catch (NumberFormatException nfe) {
+                amount = BigDecimal.ZERO;
+                // TODO: show Error Dialog
+            }
+            exchangeService.exchange(from, to, amount);
+        });
+
+        this.add(fromLabel);
+        this.add(fromPick);
+        this.add(toLabel);
+        this.add(toPick);
+        this.add(amountLabel);
+        this.add(amountField);
+        this.add(okBtn);
+
+
+        this.setVisible(true);
     }
+
+    private void initAppearance() {
+        try {
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        } catch (Exception ignored) {}
+
+        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        this.setSize(600, 400);
+        this.setLayout(new GridLayout(4, 2));
+        this.setLocationRelativeTo(null);
+
+        this.setResizable(false);
+    }
+
+    private static class ValutaComboBox extends JComboBox<ValutaId> {
+
+        private ValutaComboBox() {
+            super(ValutaId.values());
+        }
+
+        private ValutaId getSelection() {
+            return (ValutaId) getSelectedItem();
+        }
+    }
+
+
 }
