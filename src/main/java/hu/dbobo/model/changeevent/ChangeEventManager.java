40b76e5248a21e4e3f322173a606af96fc64089c
AdamBalazsKovacs <dbobo4@gmail.com>
2022-09-05 21:38:27 +0200

    insertEvent updated

--- src/main/java/hu/dbobo/model/changeevent/ChangeEventManager.java
+++ src/main/java/hu/dbobo/model/changeevent/ChangeEventManager.java
@@ -14,16 +14,15 @@ public class ChangeEventManager {
     public void insertEvent(ChangeEvent changeEvent) {
         String sql = """
                 INSERT INTO change_event (change_id, valuta_from, valuta_to, amount, change_date)
-                VALUES (?,?,?,?,?)
+                VALUES (NEXT VALUE FOR APP.change_id,?,?,?,?)
                 """;
         try (Connection connection = getConnection();
              PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
 
-            preparedStatement.setInt(1, changeEvent.getChange_id());
-            preparedStatement.setString(2, changeEvent.getValuta_from().toString());
-            preparedStatement.setString(3, changeEvent.getValuta_to().toString());
-            preparedStatement.setDouble(4, changeEvent.getAmount());
-            preparedStatement.setDate(5, Date.valueOf(changeEvent.getChange_date()));
+            preparedStatement.setString(1, changeEvent.getValuta_from().toString());
+            preparedStatement.setString(2, changeEvent.getValuta_to().toString());
+            preparedStatement.setDouble(3, changeEvent.getAmount());
+            preparedStatement.setDate(4, Date.valueOf(changeEvent.getChange_date()));
 
             preparedStatement.executeUpdate();
 
